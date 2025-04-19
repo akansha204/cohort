6ada0d24d228d17505a190a51b9420354d012424
akansha204 <akansharoy2008@gmail.com>
2025-04-19 21:18:25 +0530

    postgressConnectivity

--- postgress-demo/src/index.ts
+++ postgress-demo/src/index.ts
@@ -1,13 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import {Client} from 'pg';
 
 // const pgClient = new Client("connectiom string"); one way
 
 const pgClient = new Client({
-    user:"neondb_owner",
-    password:"npg_HQz6cFYMe3Ps",
-    port:5432,
-    host:"ep-old-band-a4ny0cxq-pooler.us-east-1.aws.neon.tech",
-    database:"neondb",
+    user: process.env.POSTGRES_USER,
+    password:process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT ? Number(process.env.POSTGRES_PORT) : undefined,
+    host:process.env.POSTGRES_HOST,
+    database:process.env.POSTGRES_DB,
     ssl: true,
 })
 
