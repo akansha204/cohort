49265318f060214889e74a91175fb36cb217d461
akansha204 <akansharoy2008@gmail.com>
2025-04-20 21:03:54 +0530

    sqlInjection

--- postgress-demo/src/index.ts
+++ postgress-demo/src/index.ts
@@ -1,7 +1,10 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import {Client} from 'pg';
+import express from 'express';
+const app = express();
 
+app.use(express.json());
 // const pgClient = new Client("connectiom string"); one way
 
 const pgClient = new Client({
@@ -12,10 +15,32 @@ const pgClient = new Client({
     database:process.env.POSTGRES_DB,
     ssl: true,
 })
+pgClient.connect();
 
-async function main() {
-    await pgClient.connect();
-    const res = await pgClient.query("UPDATE users SET username = 'Akansha Roy' WHERE id = 1;");
-    console.log(res.rows);
-}
-main();
+// async function main() {
+//     await pgClient.connect();
+//     const res = await pgClient.query("UPDATE users SET username = 'Akansha Roy' WHERE id = 1;");
+//     console.log(res.rows);
+// }
+// main();
+
+app.post('/signup', async (req, res) => {
+    const {username, password,email} = req.body;
+    try {
+        const insertQuery = `INSERT INTO users (username, password, email) VALUES ($1, $2, $3) ;` // to prevent sql injection
+        const response = await pgClient.query(insertQuery, [username, password, email]);
+        res.json({
+            message:"User created successfully",
+        })
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({
+            message: "Error creating user",
+        })
+        
+    }
+
+    
+})
+app.listen(3000);
+
