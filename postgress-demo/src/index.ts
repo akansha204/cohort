0eb89f09b41c477b666cf2a3f3f3191db9d14dd6
akansha204 <akansharoy2008@gmail.com>
2025-04-24 14:55:27 +0530

    done till joins

--- postgress-demo/src/index.ts
+++ postgress-demo/src/index.ts
@@ -26,9 +26,22 @@ pgClient.connect();
 
 app.post('/signup', async (req, res) => {
     const {username, password,email} = req.body;
+
+    const{city, country, street,pincode} = req.body;
     try {
-        const insertQuery = `INSERT INTO users (username, password, email) VALUES ($1, $2, $3) ;` // to prevent sql injection
+        const insertQuery = `INSERT INTO users (username, password, email) VALUES ($1, $2, $3) RETURNING id ;` // to prevent sql injection
+        const addressInsertQuery = `INSERT INTO addresses (city, country, street, pincode, user_id) VALUES ($1, $2, $3, $4, $5);` 
+
+        //transaction concept of dbms
+        await pgClient.query("BEGIN;");
+
         const response = await pgClient.query(insertQuery, [username, password, email]);
+        const userId = response.rows[0].id;
+        const addressResponse = await pgClient.query(addressInsertQuery, [city, country, street, pincode, userId]);
+
+        await pgClient.query("COMMIT;");
+
+
         res.json({
             message:"User created successfully",
         })
@@ -39,8 +52,17 @@ app.post('/signup', async (req, res) => {
         })
         
     }
+})
 
-    
+//join concept that creates a new table by joining two tables
+app.get('/better-metadata', async (req, res) => {
+    const id = req.query.id;
+    const query = `SELECT u.id, u.username, u.email, a.city, a.country, a.street, a.pincode FROM users u JOIN addresses a ON u.id = a.user_id WHERE u.id = $1;`;
+
+    const response = await pgClient.query(query, [id]);
+    res.json({
+        response: response.rows,
+    })
 })
 app.listen(3000);
 
