58ae05aab0271916b4525882ba04f30f69033347
akansha204 <akansharoy2008@gmail.com>
2025-04-26 22:15:36 +0530

    nextTodoapp

--- todo-app/app/api/v1/signup/route.ts
+++ todo-app/app/api/v1/signup/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import prisma from '../../../lib/db' 
 
 export async function POST(req:NextRequest){
     const data = await req.json();
