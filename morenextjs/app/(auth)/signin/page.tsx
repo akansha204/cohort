80227ed7dd35a8895e6cf530a327daabe7317faa
akansha204 <akansharoy2008@gmail.com>
2025-04-27 12:17:14 +0530

    RouteGroups()

---
+++ morenextjs/app/(auth)/signin/page.tsx
@@ -0,0 +1,11 @@
+export default function SignIn() {
+    return <>
+    <div className="w-screen h-screen flex items-center justify-center ">Signin
+        <div>
+            <input type="text" placeholder="username"/>
+            <input type="text" placeholder="password"/>
+        </div>
+    </div>
+    
+    </>
+}
---
+++ morenextjs/app/(auth)/signup/page.tsx
@@ -0,0 +1,11 @@
+export default function SignUp() {
+    return <>
+    <div className="w-screen h-screen flex items-center justify-center ">Signup
+        <div>
+            <input type="text" placeholder="username"/>
+            <input type="text" placeholder="password"/>
+        </div>
+    </div>
+    
+    </>
+}
